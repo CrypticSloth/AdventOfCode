06fa4c35003c766c7a82dd27f51e38d81480114c
Erik Sorensen <sorensen.erik48@gmail.com>
2024-11-26 18:24:33 -0500

    improve input parsing

--- python/2023/2/solution.py
+++ python/2023/2/solution.py
@@ -24,14 +24,23 @@ class Solver():
             color = t[1]
             set_dict[color] = num
         return set_dict
+    
+    def parse_input(self, line:str) -> tuple:
+
+        game_number = self.parse_game_number(line)
+        parsed_sets = self.parse_game_set(line)
+        parsed_pulls = [self.parse_set(ps) for ps in parsed_sets]
+
+        return game_number, parsed_pulls
 
 
 if __name__ == "__main__":
 
     solver = Solver()
 
-    test_line = solver.lines[10]
+    test_line = solver.lines[0]
     print(test_line)
     print(solver.parse_game_number(test_line))
     print(solver.parse_game_set(test_line))
     print(solver.parse_set(solver.parse_game_set(test_line)[0]))
+    print(solver.parse_input(test_line))
